455eb11c70820b81771c4622e1762ee4e41729ee
liuxi <2937377096@qq.com>
2019-09-09 13:57:44 +0800

    尝试

---
+++ SS_Maven_Parent/SS_Maven_Dao/src/main/java/com/maven/dao/itemsMapper.java
@@ -0,0 +1,14 @@
+package com.maven.dao;
+
+import com.maven.pojo.Items;
+import com.maven.pojo.User;
+
+public interface itemsMapper {
+
+    //根据id进行查询
+    public Items findById(Integer id);
+
+
+    //添加
+    public void insert(User user);
+}
